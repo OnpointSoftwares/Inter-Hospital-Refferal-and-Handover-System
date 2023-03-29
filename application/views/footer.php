0f4a73f069ae939aa3271d6b236799e90c88ef38
OnpointSoftwares <vincentbettoh@gmail.com>
2023-03-29 13:29:52 +0300

    First commit to this repository

---
+++ application/views/footer.php
@@ -0,0 +1,5 @@
+    <div style="clear:both;color:#aaa; padding:20px;">
+
+    	<hr /><center>&copy; 2022 <a target="_blank" href="http://optimumlinkup.com.ng">Hospital Management System</a></center>
+
+    </div>
